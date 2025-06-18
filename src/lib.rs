7cd57b7f3cbbf4ce45a2f0e3cd943e3255158ccd
Adewale Ijalana <adewaleijalana@adewaleijalana-2.local>
2025-06-18 02:03:19 +0100

    using hex to get tx version

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,13 @@
 // Implement extract_tx_version function below
 pub fn extract_tx_version(raw_tx_hex: &str) -> Result<u32, String> {
-    todo!()
+    hex::decode(raw_tx_hex)
+        .map_err(|_| "Hex decode error".to_string())
+        .and_then(|bytes| {
+            if bytes.len() < 4 {
+                Err("Transaction data too short".to_string())
+            } else {
+                let version = u32::from_le_bytes([bytes[0], bytes[1], bytes[2], bytes[3]]);
+                Ok(version)
+            }
+        })
 }
